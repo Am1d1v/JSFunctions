52f9fd305a860d997780ba5ab74adf3c50460e42
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-08-03 21:34:10 +0300

    Add files via upload

--- numbers.js
+++ numbers.js
@@ -1,19 +1,32 @@
 
 
-const airline = {
+
+
+const book = function(flightNumber, passangerName){
+    console.log(`${passangerName} has booked a ticket on ${this.name} flight ${flightNumber}`);
+
+    this.bookings.push({flight: `${this.airlineCode}${flightNumber}, ${passangerName}`});
+}
+
+const airlineOne = {
     name: 'SkyAirLine',
-    airlineCode: 'Code',
+    airlineCode: 'CodeAirlineOne',
     bookings: [],
-
-    book(flightNumber, passangerName){
-        console.log(`${passangerName} has booked a ticket on ${this.name} flight ${flightNumber}`);
-
-        this.bookings.push({flight: `${this.airlineCode}${flightNumber}, ${passangerName}`});
-    }
 
 }
 
-airline.book(111, 'PersonName1 PersonSecondName1');
-airline.book(122, 'PersonName2 PersonSecondName2');
+const airlineTwo = {
+    name: 'SkyAirLine',
+    airlineCode: 'CodeAirlineTwo',
+    bookings: [],
 
-console.log(airline.bookings);
+}
+
+
+book.call(airlineOne, 121, "SomeOneName1 SomeOneSecondName1");
+book.call(airlineTwo, 124, "SomeOneName2 SomeOneSecondName2");
+
+
+
+console.log(`Airline One Bookings` + JSON.stringify(airlineOne.bookings));
+console.log(`Airline Two Bookings` + JSON.stringify(airlineTwo.bookings));
