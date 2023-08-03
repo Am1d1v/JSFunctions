71a32e31a824b1dd60ca66720eee4c5e1b16ee46
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-08-03 21:43:33 +0300

    Add files via upload

--- numbers.js
+++ numbers.js
@@ -26,7 +26,9 @@ const airlineTwo = {
 book.call(airlineOne, 121, "SomeOneName1 SomeOneSecondName1");
 book.call(airlineTwo, 124, "SomeOneName2 SomeOneSecondName2");
 
+const flightData = [111, 'SomeOneName3 SomeOneSecondName3'];
 
+book.apply(airlineOne, flightData);
 
 console.log(`Airline One Bookings` + JSON.stringify(airlineOne.bookings));
 console.log(`Airline Two Bookings` + JSON.stringify(airlineTwo.bookings));
