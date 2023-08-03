6dadc72ed63f90e62f4d87494b264cb3aa365ecf
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-08-03 19:24:40 +0300

    Add files via upload

--- numbers.js
+++ numbers.js
@@ -7,8 +7,13 @@ const airline = {
 
     book(flightNumber, passangerName){
         console.log(`${passangerName} has booked a ticket on ${this.name} flight ${flightNumber}`);
+
+        this.bookings.push({flight: `${this.airlineCode}${flightNumber}, ${passangerName}`});
     }
+
 }
 
 airline.book(111, 'PersonName1 PersonSecondName1');
 airline.book(122, 'PersonName2 PersonSecondName2');
+
+console.log(airline.bookings);
