53c8e3f37b852899019eb6c4c8e43ea4aa1ce6cb
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-08-04 20:30:59 +0300

    Add files via upload

--- numbers.js
+++ numbers.js
@@ -1,6 +1,8 @@
 
 
 
+/*
+
 
 const book = function(flightNumber, passangerName){
     console.log(`${passangerName} has booked a ticket on ${this.name} flight ${flightNumber}`);
@@ -32,3 +34,35 @@ book.apply(airlineOne, flightData);
 
 console.log(`Airline One Bookings` + JSON.stringify(airlineOne.bookings));
 console.log(`Airline Two Bookings` + JSON.stringify(airlineTwo.bookings));
+
+*/
+
+const book = function(flightNumber, passangerName){
+    console.log(`${passangerName} has booked a ticket on ${this.name} flight ${flightNumber}`);
+
+    this.bookings.push({flight: `${this.airlineCode}${flightNumber}, ${passangerName}`});
+}
+
+
+const airlineOne = {
+    name: 'SkyAirLine',
+    airlineCode: 'CodeAirlineOne',
+    bookings: [],
+
+    book: function(flightNumber, passangerName){
+        console.log(`${passangerName} has booked a ticket on ${this.name} flight ${flightNumber}`);
+    
+        this.bookings.push({flight: `${this.airlineCode}${flightNumber}, ${passangerName}`});
+    }
+    
+
+}
+
+const airlineTwo = {
+    name: 'SkyAirLine',
+    airlineCode: 'CodeAirlineTwo',
+    bookings: [],
+}
+
+const bookedAirline2 = book.bind(airlineTwo);
+bookedAirline2(456, 'Name1 Surname1');
