049649ed5ca434b81493b5e82c7c2932c4292402
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-08-05 18:13:53 +0300

    Add files via upload

--- set.js
+++ set.js
@@ -10,3 +10,15 @@ const orders = new Set([
 
 
 console.log(orders);
+console.log(orders.has('Sushi'));
+console.log(orders.has('Pizza'));
+
+orders.add('Some Food');
+//console.log(new Set('Hello'));
+
+for (const order of orders){
+    console.log(order);
+}
+
+const foodOrder = [...orders];
+console.log(foodOrder);
